4fe8cad0a58f1a3ef06c841e9dc5d5019326c93d
soultaco83 <soulnacho@gmail.com>
2025-02-13 03:17:14 -0500

    reset to master

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import TabbedView from '../components/tabbedview/tabbedview';
 import globalize from '../lib/globalize';
 import '../elements/emby-tabs/emby-tabs';
@@ -29,6 +30,8 @@ class HomeView extends TabbedView {
             name: globalize.translate('Home')
         }, {
             name: globalize.translate('Favorites')
+        }, {
+            name: globalize.translate('Requests')
         }];
     }
 
@@ -46,6 +49,11 @@ class HomeView extends TabbedView {
 
             case 1:
                 depends = 'favorites';
+                break;
+
+            case 2:
+                depends = 'requests';
+                break;
         }
 
         const instance = this;
@@ -63,3 +71,77 @@ class HomeView extends TabbedView {
 }
 
 export default HomeView;
+=======
+import TabbedView from '../components/tabbedview/tabbedview';
+import globalize from '../lib/globalize';
+import '../elements/emby-tabs/emby-tabs';
+import '../elements/emby-button/emby-button';
+import '../elements/emby-scroller/emby-scroller';
+import LibraryMenu from '../scripts/libraryMenu';
+
+class HomeView extends TabbedView {
+    setTitle() {
+        LibraryMenu.setTitle(null);
+    }
+
+    onPause() {
+        super.onPause(this);
+        document.querySelector('.skinHeader').classList.remove('noHomeButtonHeader');
+    }
+
+    onResume(options) {
+        super.onResume(this, options);
+        document.querySelector('.skinHeader').classList.add('noHomeButtonHeader');
+    }
+
+    getDefaultTabIndex() {
+        return 0;
+    }
+
+    getTabs() {
+        return [{
+            name: globalize.translate('Home')
+        }, {
+            name: globalize.translate('Favorites')
+        }, {
+            name: globalize.translate('Requests')
+        }];
+    }
+
+    getTabController(index) {
+        if (index == null) {
+            throw new Error('index cannot be null');
+        }
+
+        let depends = '';
+
+        switch (index) {
+            case 0:
+                depends = 'hometab';
+                break;
+
+            case 1:
+                depends = 'favorites';
+                break;
+
+            case 2:
+                depends = 'requests';
+                break;
+        }
+
+        const instance = this;
+        return import(/* webpackChunkName: "[request]" */ `../controllers/${depends}`).then(({ default: ControllerFactory }) => {
+            let controller = instance.tabControllers[index];
+
+            if (!controller) {
+                controller = new ControllerFactory(instance.view.querySelector(".tabContent[data-index='" + index + "']"), instance.params);
+                instance.tabControllers[index] = controller;
+            }
+
+            return controller;
+        });
+    }
+}
+
+export default HomeView;
+>>>>>>> 3f383f10c94cc67980bf4904f86078a15f73d087
